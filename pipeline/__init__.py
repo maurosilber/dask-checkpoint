c5b8e3bac3ece0e2f9bacb8ea86f55922942f7e9
Mauro Silberberg <maurosilber@gmail.com>
2021-04-20 20:01:22 -0300

    Removed context module

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,5 +1,4 @@
 from . import config  # noqa: F401
-from .context import Load, Save
 from .task import Task, dependency
 
-__all__ = ["Load", "Save", "Task", "dependency"]
+__all__ = ["Task", "dependency"]
