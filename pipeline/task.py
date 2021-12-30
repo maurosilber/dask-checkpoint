e55882bad5e28b0bcd7a521664d4133a7cf99726
Mauro Silberberg <maurosilber@gmail.com>
2021-12-30 01:58:29 -0300

    Reimplemented Task
    Task are dataclasses which contain a run (static)method that defines
    the computation to perform. This method's parameters must be:
    - class annotations, to be passed when instantiating the task, and/or
    - class attributes or depedency-decorated methods, which act as default values.
    
    When instantiating a Task subclass, it actually returns a dask.delayed, that
    can be passed to other Tasks or computed with task.compute()

--- pipeline/task.py
+++ pipeline/task.py
@@ -1,81 +1,14 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from functools import cached_property
-from inspect import Parameter, Signature, signature
+from inspect import BoundArguments, Parameter, signature
 from typing import Generic, TypeVar
 
-import dask
+from dask import delayed
 from dask.base import tokenize
-from dask.optimization import cull
-from dask.utils import ensure_dict
-
-from .serializer import Serializer
-from .storage import Storage, storage_ctx
 
 T = TypeVar("T")
-
-
-def _optimize_no_storage(dsk, keys):
-    """Replace Task instances with Task.run function,
-    and remove unnecesary tasks not required to calculate keys.
-    """
-    dsk, _ = cull(dsk, keys)
-    for k, v in dsk.items():
-        task = v[0]
-        if isinstance(task, Task):
-            dsk[k] = (task.run, *v[1:])
-    return dsk
-
-
-def _optimize_with_storage(dsk, keys, storage):
-    """Replace Task instances with either:
-        - Task.run function
-        - Load function
-        - Task.run & Save functions
-    and remove unnecesary tasks not required to calculate keys.
-    """
-    dsk, _ = cull(dsk, keys)
-
-    for k, v in dsk.items():
-        task = v[0]
-
-        if not isinstance(task, Task):
-            continue
-
-        if not task._save:
-            dsk[k] = (task.run, *v[1:])
-            continue
-
-        if storage.exists(task.key):
-            dsk[k] = (task._build_load_func(), storage.read(task.key))
-        else:
-            # TODO: What if the scheduler runs the task more than once?
-            dsk[k] = (
-                task._build_dump_func(),
-                storage.write(task.key),
-                (task.run, *v[1:]),
-            )
-
-    # Remove unnecesary dependencies replaced by loads
-    dsk, _ = cull(dsk, keys)
-    return dsk
-
-
-def _optimize(dsk, keys):
-    """Replace Task instances with Task.run, loading,
-    or Task.run and saving functions, and remove
-    unnecesary tasks not required to calculate keys.
-    """
-    dsk = ensure_dict(dsk)
-    storage: Storage = storage_ctx.get(None)
-    if storage is None:
-        dsk = _optimize_no_storage(dsk, keys)
-    else:
-        dsk = _optimize_with_storage(dsk, keys, storage)
-    return dsk
-
-
-dask.config.set(delayed_optimize=_optimize)
 
 
 class dependency(Generic[T]):
@@ -91,119 +24,108 @@ class dependency(Generic[T]):
         return self.func(obj)
 
 
-class Save:
-    serializers: tuple[Serializer, ...] = ()
+class Task(Generic[T]):
+    __bound: BoundArguments
 
     @staticmethod
-    def dumps(x):
-        return x
-
-    @staticmethod
-    def loads(x):
-        return x
-
-    @classmethod
-    def _build_dump_func(cls):
-        serializers = (cls.dumps, *(s.dumps for s in cls.serializers))
-
-        def _dump(stream, result):
-            out = result
-            for func in serializers:
-                out = func(out)
-
-            with stream as s:
-                s.write(out)
-
-            return result
-
-        return _dump
-
-    @classmethod
-    def _build_load_func(cls):
-        serializers = (cls.loads, *(s.loads for s in cls.serializers))
-
-        def _load(stream):
-            with stream as s:
-                result = s.read()
-
-            for func in serializers[::-1]:
-                result = func(result)
-
-            return result
-
-        return _load
-
-
-class Task(Save):
-    def __init_subclass__(cls, save=False):
-        """Check run and build task signatures.
-
-        Run signature must not have keyword-only parameters.
-
-        Task signature:
-            Run method -> positional parameters
-            Dependencies -> keyword-only parameters
-        """
-
-        # Check run signature
-        run_params = signature(cls.run).parameters
-        kinds = (Parameter.KEYWORD_ONLY, Parameter.VAR_KEYWORD)
-        if any(p.kind in kinds for p in run_params.values()):
-            raise Exception("run parameters must be positional, not keyword-only.")
-
-        # Build task signature
-        task_params = run_params.copy()
-
-        for name in cls._dependencies():
-            if name not in task_params:
-                task_params[name] = Parameter(name, Parameter.KEYWORD_ONLY)
-
-        cls.__signature__ = Signature(task_params.values())
-        cls._save = save
-
-    @classmethod
-    def _dependencies(cls):
-        return (d for d in dir(cls) if isinstance(getattr(cls, d), dependency))
-
-    @staticmethod
-    def run():
+    def run() -> T:
         """Compute the result.
 
-        Run signature must have positional parameters, which are
-        declared as class attributes or dependency methods.
+        Parameter names in run signature must be at least one of:
+            - class annotation
+            - class attribute
+            - @dependency-decorated method
         """
         raise NotImplementedError
 
     @cached_property
-    def key(self) -> str:
+    def dask_key(self) -> str:
+        """Unique name for a given Task.
+
+        It is the name used for the dask graph and to store the result.
+
+        By default: {Task name}/{hash from run parameters}
+        """
         name = self.__class__.__qualname__
-        return f"{name}-{tokenize(*self._run_args)}"
+
+        args, kwargs = self._run_params
+        hash = tokenize(*args, **kwargs)
+
+        return f"{name}/{hash}"
+
+    def __init_subclass__(cls):
+        """Validates that a Task is well-specified."""
+
+        # Convert to dataclass.
+        dataclass(cls)
+
+        # Validate run method:
+        #   - convert to a staticmethod
+        #   - check that all run parameters either exist as dependencies
+        #     or are in __annotations__.
+        cls.run = staticmethod(cls.run)
+        missing_parameters = {
+            k
+            for k in signature(cls.run).parameters
+            if k not in cls.__annotations__ and not hasattr(cls, k)
+        }
+        if len(missing_parameters) > 0:
+            raise NameError(
+                f"{cls}.run method has parameters with are neither an"
+                f"annotation or a dependecy: {missing_parameters}"
+            )
 
     def __new__(cls, *args, _delayed=True, **kwargs):
+        # If _delayed=True, we return a task instance wrapped in dask.delayed.
+
+        if cls is Task:
+            raise NotImplementedError
+
         # Create instance
         self = super().__new__(cls)
 
-        # Initialize instance attributes.
-        self._bound = cls.__signature__.bind_partial(*args, **kwargs)
-        for name, value in self._bound.arguments.items():
+        # Initialize instance attributes. Dependencies are overriden as they are
+        # non-data descriptors.
+        self.__bound = signature(self.__init__).bind_partial(*args, **kwargs)
+        for name, value in self.__bound.arguments.items():
             setattr(self, name, value)
 
         if _delayed:
-            return dask.delayed(self, traverse=False)(
-                *self._run_args, dask_key_name=self.key
-            )
+            # Return the task instance as a dask.delayed function,
+            # called with the task.run parameters.
+            func = delayed(self, pure=True, traverse=False)
+            args, kwargs = self._run_params
+            return func(*args, **kwargs, dask_key_name=self.dask_key)
         else:
             # Return instance. Needed for Task serialization.
             return self
 
     def __getnewargs_ex__(self):
-        return self._bound.args, {**self._bound.kwargs, "_delayed": False}
-
-    @cached_property
-    def _run_args(self):
-        parameters = signature(self.run).parameters
-        return tuple(getattr(self, k) for k in parameters)
+        # Enables task serialization. We need to build a Task instance,
+        # not a dask.delayed function, hence the _delayed=False
+        return self.__bound.args, {**self.__bound.kwargs, "_delayed": False}
 
     def __call__(self, *args, **kwargs):
-        # Needed for dask to consider instances as (dask) tasks.
-        raise NotImplementedError
+        # Dask considers callables as tasks in its graph.
+        # Hence, we can pass the task itself as the callable,
+        # and later have access to dask_key, encode and decode.
+        return self.run(*args, **kwargs)
+
+    @cached_property
+    def _run_params(self) -> tuple[tuple, dict]:
+        # Get run function parameters from its signature, and
+        # collect them from instance or class attributes/dependencies,
+        args, kwargs = [], {}
+        for name, param in signature(self.run).parameters.items():
+            value = getattr(self, name)
+
+            if param.kind <= Parameter.POSITIONAL_OR_KEYWORD:
+                args.append(value)
+            elif param.kind == Parameter.VAR_POSITIONAL:
+                args.extend(value)  # value is an iterable
+            elif param.kind == Parameter.KEYWORD_ONLY:
+                kwargs[name] = value
+            else:
+                kwargs.update(value)  # value is a mapping
+
+        return args, kwargs
