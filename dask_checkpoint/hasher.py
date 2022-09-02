5dad04ae57545fa7efe04b2ed8da809843cc2496
Mauro Silberberg <maurosilber@gmail.com>
2022-09-02 15:42:20 -0300

    Add docstrings and typing

--- dask_checkpoint/hasher.py
+++ dask_checkpoint/hasher.py
@@ -14,16 +14,32 @@ FunctionHasher: TypeAlias = Callable[[Callable], str]
 ArgumentHasher: TypeAlias = Callable[[dict], str]
 
 
-def function_name(func):
-    """Function name with a trailing slash."""
+def function_name(func: FunctionType) -> str:
+    """Function name with a trailing slash.
+
+    >>> def func(x):
+    ...     return 2 * x
+    >>> function_name(func)
+    'func/'
+    """
     return f"{func.__name__}/"
 
 
-def tokenize(kwargs):
+def tokenize(kwargs: dict) -> str:
     return dask_tokenize(**kwargs)
 
 
-def exclude(*names: str, hasher: ArgumentHasher = tokenize):
+def exclude(*names: str, hasher: ArgumentHasher = tokenize) -> ArgumentHasher:
+    """Generates a hasher function which excludes some parameters.
+
+    >>> from dask_checkpoint import task
+    >>> @task(hasher=exclude("x"))
+    ... def func(x, y):
+    ...     return x + y
+    >>> func(1, 0).key == func(2, 0).key
+    True
+    """
+
     def exclude_hasher(kwargs):
         for name in names:
             del kwargs[name]
@@ -32,7 +48,7 @@ def exclude(*names: str, hasher: ArgumentHasher = tokenize):
     return exclude_hasher
 
 
-def function_hash(func: FunctionType):
+def function_hash(func: FunctionType) -> str:
     """Function name and a hash of its bytecode,
     with a trailing slash.
 
